afe363e6a2372c6c6ea698d38785552a815c0a78
Misael-GC <misael.gomez1623@gmail.com>
2022-06-12 23:06:22 -0500

    Migración a Axios

--- src/main.js
+++ src/main.js
@@ -4,10 +4,20 @@
 //{time_window}: cuando queremos ver las tendencias; day 
 //no olvides enviar tu api key;?api_key=
 
+const api = axios.create({
+    baseURL:'https://api.themoviedb.org/3/',
+    headers:{
+        'Content-Type': 'application/json;charset=utf-8',
+    },
+    params:{
+        'api_key':API_KEY,
+    },
+})
+
 async function getTrendingMoviesPreview(){
-    const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
-    const data = await res.json();
+    const {data} = await api ('trending/movie/day');
     const movies = data.results;
+    
     movies.forEach(movie => {
         const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
         const movieContainer = document.createElement('div');
@@ -24,8 +34,7 @@ async function getTrendingMoviesPreview(){
 }
 
 async function getCategoriesPreview(){
-    const res = await fetch(`https://api.themoviedb.org/3//genre/movie/list?api_key=${API_KEY}`)
-    const data = await res.json();
+    const { data } = await api('genre/movie/list');
 
     const categories = data.genres;
     categories.forEach(category => {
